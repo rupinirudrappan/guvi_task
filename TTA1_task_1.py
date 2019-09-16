778dc537edb1ba9e8a954bd710770e874319e767
RupiniRudrappan <46262915+rupinirudrappan@users.noreply.github.com>
2019-09-16 18:54:37 +0530

    Add files via upload

---
+++ TTA1_task_1.py
@@ -0,0 +1,30 @@
+import collections
+
+input_list=[3,5,[3,56],3,'guvi',[3,7,'zen',4.6],5,2,'pit']
+
+max=0
+
+sub_list=[]
+
+for i in input_list:
+
+	if isinstance(i, collections.Iterable) and not isinstance(i, (str)):
+		sub_list.append(i)
+
+print(len(sub_list))
+
+max_list=[]
+
+for i in sub_list:
+
+	if max<len(i):
+
+		max=len(i)
+		max_list=i
+
+print(max_list)
+
+print(input_list.index(max_list))
+
+
+    
